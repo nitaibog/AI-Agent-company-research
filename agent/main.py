7ea53ae74bcefff998fa65d94a0d280563e95c1a
nitaibog <nitaibogner@gmail.com>
2025-02-01 16:15:10 +0200

    search and write report added

--- agent/main.py
+++ agent/main.py
@@ -1,7 +1,8 @@
+import asyncio
+import operator
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage, SystemMessage
 from langgraph.graph.message import AnyMessage, add_messages
-from utils.tools import search_web
 from typing import Any, List, Optional
 from typing_extensions import TypedDict
 from pydantic import BaseModel, Field
@@ -17,8 +18,8 @@ from langchain_core.messages import AnyMessage
 from langchain_core.runnables import RunnableConfig
 from langgraph.graph import MessagesState
 from typing import Annotated, List
-from utils.configuration import Configuration
-from utils.prompts import QUERY_WRITER_PROMPT
+from utils.prompts import QUERY_WRITER_PROMPT, INFO_PROMPT
+from tavily import AsyncTavilyClient
 import os, getpass
 
 
@@ -42,6 +43,8 @@ class AgentState(MessagesState):
     search_queries: list[str] = Field(
         description="List of search queries.",
     )
+    search_results:  list[str] #Annotated[list, operator.add] # Source docs
+    
     pass
 
 
@@ -50,12 +53,11 @@ class Queries(BaseModel):
         description="List of search queries.",
     )
 
-
 def generate_queries(state: AgentState):
     """Generate search queries based on the user input and extraction schema."""
     # Get configuration
     # configurable = Configuration.from_runnable_config(config)
-    max_search_queries = 6
+    max_search_queries = 4
 
     # Generate search queries
     structured_llm = llm.with_structured_output(Queries)
@@ -73,19 +75,89 @@ def generate_queries(state: AgentState):
     query_list = [query for query in results.queries]
     return {"search_queries": query_list}
 
+async def search_web(state: AgentState):
+        tavily_search = AsyncTavilyClient()
+        search_tasks = []
+        for query in state["search_queries"]:
+             search_tasks.append(tavily_search.search(query,max_results=3))
+        search_results = await asyncio.gather(*search_tasks) #list[dict]
+        return{"search_results" : search_results}
+
+def format_search_results(search_results: list[dict] ):
+    sources_list = []
+    for result in search_results:
+        sources_list.extend(result["results"])
+    formatted_text = "Sources:\n\n"
+    for source in sources_list:
+        formatted_text += f"Source {source['title']}:\n===\n"
+        formatted_text += f"URL: {source['url']}\n===\n"
+        formatted_text += (
+            f"Most relevant content from source: {source['content']}\n===\n"
+        )
+    return formatted_text
+
+
+
+def write_report(state: AgentState):
+    search_results = state["search_results"]
+    search_results_str = format_search_results(search_results)
+    answer_prompt = INFO_PROMPT.format(
+        company=state["company"],
+        queries=state["queries"],
+        user_notes=state["messages"],
+        content=search_results_str
+    )
+    report = llm.invoke([SystemMessage(content=answer_prompt)])
+    print(report.content)
+    return{"messages" : report}
+
+
 builder = StateGraph(AgentState)
 
 builder.add_node("generate_queries" , generate_queries)
+builder.add_node("search_web", search_web)
+builder.add_node("write_report", write_report)
 
 builder.add_edge(START, "generate_queries")
-builder.add_edge("generate_queries", END)
+builder.add_edge("generate_queries", "search_web")
+builder.add_edge("search_web", "write_report")
+builder.add_edge("write_report", END)
 
 memory = MemorySaver()
 
 graph = builder.compile(checkpointer=memory)
 
-display(Image(graph.get_graph(xray=1).draw_mermaid_png()))
+# display(Image(graph.get_graph(xray=1).draw_mermaid_png()))
 
+
+# company = "Tesla"
+# thread = {"configurable": {"thread_id": "3"}}
+async def main():
+    company = "Tesla"
+    thread = {"configurable": {"thread_id": "5"}}
+
+    async for event in graph.astream(
+        {"company": company, "user_notes": "please provide data about the company's stock and growth potential"}, 
+        thread, 
+        stream_mode="values"
+    ):
+        # Review search queries
+        search_queries = event.get("search_queries", "")
+        if search_queries:
+            for query in search_queries:
+                print(f"search_queries: {query}")
+                print("-" * 50)
+
+        # # Review search results
+        # search_results = event.get("search_results", "")
+        # if search_results:
+        #     for result in search_results:
+        #         print(f"search_results: {result}")
+        #         print("-" * 50)
+
+# Run the async function
+if __name__ == "__main__":
+    asyncio.run(main())
 
 # def search_web(state: AgentState):
     
--- agent/utils/prompts.py
+++ agent/utils/prompts.py
@@ -24,3 +24,27 @@ Do not include any dates in your queries
 
 Create a focused query that will maximize the chances of finding relevant information."""
 
+
+INFO_PROMPT = """You are doing web research on a company, {company}. 
+
+
+You have just scraped website content. Your task is to take clear, organized notes about the company.
+
+The following qustions shows the type of information we're interested in:
+{queries}
+
+<Website contents>
+{content}
+</Website contents>
+
+
+
+Please provide detailed research notes that:
+1. Are well-organized and easy to read
+2. Focus on topics mentioned in the schema
+3. Include specific facts, dates, and figures when available
+4. Maintain accuracy of the original content
+5. Note when important information appears to be missing or unclear
+
+Remember: Don't try to format the output to match the schema - just take clear notes that capture all relevant information."""
+
